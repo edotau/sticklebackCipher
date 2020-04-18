7dc19469ef9e65139ed417dce42c01f86ad956cc
edotau <ericdotau@gmail.com>
2020-04-18 03:58:33 -0400

    removed extra script that should not be there

--- gatk/sticklebackGenotypeVcf.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -e
-module add GATK/4.1.3.0-gcb01
-gatk --java-options "-Xmx16g" GenotypeGVCFs -R /data/lowelab/edotau/toGasAcu2RABS/gasAcu2RABS/gasAcu2RABS.fasta -V $1 -O $(basename $1 .g.vcf)GenotypeGVCFs.vcf
