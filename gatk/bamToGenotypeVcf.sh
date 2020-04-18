686d0e7656498c8105ef4472c6f31637ec6fc12c
edotau <eric.au@duke.edu>
2020-04-18 10:59:45 -0700

    add slurm job commands

--- gatk/bamToGenotypeVcf.sh
+++ gatk/bamToGenotypeVcf.sh
@@ -1,21 +1,26 @@
 #!/bin/sh -e
-#Either load modules or assign paths to GATK, samtools, and picard tools
-module load GATK/4.1.3.0-gcb01
+#SBATCH --mem=16G
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --nodes=1
+#SBATCH --job-name=toGenotypeVcf
 
 BAM=$1
 PREFIX=$(basename $BAM .bam)
 
+#Either load modules or assign paths to GATK, samtools, and picard tools
+module load GATK/4.1.3.0-gcb01
 #Set your reference:
 REF=/data/lowelab/edotau/toGasAcu2RABS/gasAcu2RABS/gasAcu2RABS.fasta
 #if you want to name your files anything else set these variables
 
 DIR=${PREFIX}"_genotypeVcfs"
 mkdir -p $DIR
-nodups=$DIR/${PREFIX}.nodups.bam
+markedDups=$DIR/${PREFIX}.markedDups.bam
 output=$DIR/${PREFIX}.gatk.valid.bam
 gVcf=$DIR/${PREFIX}.wgs.g.vcf.gz
 
-gatk --java-options "-Xmx4G" MarkDuplicates -I $BAM -O $nodups -M $DIR/${PREFIX}".dup.metrics" -VALIDATION_STRINGENCY SILENT -CREATE_INDEX true
+gatk --java-options "-Xmx4G" MarkDuplicates -I $BAM -O $markedDups -M $DIR/${PREFIX}".dup.metrics" -VALIDATION_STRINGENCY SILENT -CREATE_INDEX true
 #adds Read group information to bam alignments
 #this is how GATK differentiates between your cohort of samples
 
@@ -25,11 +30,11 @@ platform="Illumina"
 unit="HiSeqX"
 sampleID=$PREFIX
 
-gatk --java-options "-Xmx4G" AddOrReplaceReadGroups -I $nodups -O /dev/stdout -LB $library -PL $platform -PU $unit -SM $PREFIX | gatk --java-options "-Xmx4G" FixMateInformation -I /dev/stdin -O $output -ADD_MATE_CIGAR true -IGNORE_MISSING_MATES true
+gatk --java-options "-Xmx8G" AddOrReplaceReadGroups -I $markedDups -O /dev/stdout -LB $library -PL $platform -PU $unit -SM $PREFIX | gatk --java-options "-Xmx4G" FixMateInformation -I /dev/stdin -O $output -ADD_MATE_CIGAR true -IGNORE_MISSING_MATES true
 samtools index $output
 
 #Now run GATK on processed BAM
-gatk HaplotypeCaller --java-options "-Xmx16G" \
+gatk HaplotypeCaller --java-options "-Xmx12G" \
 	-I $output -O $gVcf -R $REF \
 	-ERC GVCF --native-pair-hmm-threads 8 \
-rm $nodups ${nodups}.bai
+rm $markedDups ${markedDups}.bai
--- gatk/combineGVCF.sh
+++ gatk/combineGVCF.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
+#SBATCH --mem=16G
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --nodes=1
+#SBATCH --job-name=mergeGenotypeVcf
+
 module load GATK/4.1.3.0-gcb01
 PREFIX=$1
 #Write to file, then remove it to make sure it did not contain samples from a different run.
 samples=${PREFIX}.list
 echo $PREFIX >> $samples
 rm $samples
-for i in *.g.vcf
+for i in *.g.vcf.gz
 do
 	echo $i >> $samples
 done
@@ -13,3 +19,4 @@ gatk CombineGVCFs --java-options "-Xmx8g" \
 	-R /data/lowelab/edotau/toGasAcu2RABS/gasAcu2RABS/gasAcu2RABS.fasta \
 	-V $samples \
 	-O ${PREFIX}.merged.vcf.gz
+rm $samples
