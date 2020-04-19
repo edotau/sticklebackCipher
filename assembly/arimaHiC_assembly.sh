ddab52a937feff7fd46b63f9ab51651053dbe098
edotau <eric.au@duke.edu>
2020-04-19 00:59:47 -0700

    removed code

--- assembly/arimaHiC_assembly.sh
+++ assembly/arimaHiC_assembly.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
-
 REF=$1
 READ1=$2
 READ2=$3
-
-PREFIX1=$(echo $READ1 | sed 's/.fastq.gz//')
-PREFIX2=$(echo $READ2 | sed 's/.fastq.gz//')
-
 bwa_filter=/data/lowelab/edotau/sticklebackCipher/assembly/bwa_filter.sh
-ji1=$(sbatch $bwa_filter $READ1 $REF)
+jid1=$(sbatch $bwa_filter $READ1 $REF)
 jid2=$(sbatch $bwa_filter $READ2 $REF)
 
 mappingFilter=/data/lowelab/edotau/scripts/mappingQuality.sh
