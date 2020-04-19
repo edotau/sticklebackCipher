232b3e9585fb131aba6b86f5b74d70b673167fc8
edotau <ericdotau@gmail.com>
2020-04-19 01:51:37 -0400

    updated atac pipeline

--- atac/atacPeaks.sh
+++ atac/atacPeaks.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
+#SBATCH --mem=16G
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --nodes=1
+#SBATCH --parsable
+#SBATCH --time=1-00:00:00
 BAM=$1
-PREFIX=$(basename $BAM .sorted.bam.sorted)Pvalue
+PREFIX=$(basename $BAM .bam)
+qNameSort=${PREFIX}.name.sorted.bam
+samtools sort -@ 8 -n -o $qNameSort $BAM
 q=(0.05 0.01)
 a=(200 300)
 picard=/data/lowelab/edotau/software/picard.jar
 module add samtools java/1.8.0_45-fasrc01
-java -jar $picard FixMateInformation I=$BAM ADD_MATE_CIGAR=true IGNORE_MISSING_MATES=true ASSUME_SORTED=true
-Genrich=/data/lowelab/software/Genrich-master/Genrich
-$Genrich -t $BAM -o ${PREFIX}.tmp -j -y -r -v -D
+java -jar $picard FixMateInformation I=$qNameSort ADD_MATE_CIGAR=true IGNORE_MISSING_MATES=true
+peakCaller=/data/lowelab/software/Genrich-master/Genrich
+$peakCaller -t $qNameSort -o ${PREFIX}.tmp -j -y -r -v -D
 sort -k1,1 -k2,2n ${PREFIX}.tmp > ${PREFIX}.bed
--- atac/bamToBw.sh
+++ atac/bamToBw.sh
@@ -1,14 +1,19 @@
-#!/bin/sh
+#!/bin/sh -e
+#SBATCH --mem=16G
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --nodes=1
+#SBATCH --parsable
+#SBATCH --time=1-00:00:00
 module load bwa samtools
 module load bedtools2/2.25.0-fasrc01
 module load kentUtils/v302-gcb01
 module add bedtools2/2.25.0-fasrc01
-SIZE=/data/lowelab/RefGenomes/gasAcu1/gasAcu1.sizes
-REF=/data/lowelab/RefGenomes/gasAcu1/gasAcu1.fa
 BAM=$1
+REF=$2
 PREFIX=$(echo $BAM | sed 's/.bam//')
 bed=${PREFIX}.bed
 bamToBed -i $BAM > $bed
-genomeCoverageBed -bg -i $bed -g $SIZE > ${PREFIX}.bg
+faSize -detailed $REF | genomeCoverageBed -bg -i $bed -g /dev/stdin > ${PREFIX}.bg
 bedGraphToBigWig ${PREFIX}.bg $SIZE ${PREFIX}.bw
-rm ${PREFIX}.bg
+rm ${PREFIX}.bg $bed
