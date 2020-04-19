ee8cd4f728dd80cdac5c9a96ed0078c7681bdd2c
edotau <ericdotau@gmail.com>
2020-04-19 00:20:44 -0400

    longranger

--- assembly/longranger.sh
+++ assembly/longranger.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+#SBATCH --mem=64G
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --nodes=1
+#SBATCH --parsable
+#Requires name for output folder (prefix), a reference to align to, and a directory for where the 10x fastqs live
+name=$1
+REF=$2
+DIR=$3
 longranger=/data/lowelab/edotau/software/longranger-2.2.2/longranger
-longranger align --id=rabs688_10x_pacbio_aln --reference=/data/lowelab/edotau/data/pacbio_rabs_688/canu_analysis/pacbio_assembly_rabs688/refdata-pacbioContigs_rabs688 --fastqs=/data/lowelab/edotau/data/C101HW18111772/raw_data/S688_10x
+longranger align --id=$name --reference=$REF --fastqs=$DIR
