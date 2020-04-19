8cb8235c1a00c97bc0701fffbd542d7255b27f85
edotau <eric.au@duke.edu>
2020-04-19 16:19:27 -0700

    wrap fastq to genotype Vcf

--- gatk/fqToGenotypeVcfWrap.sh
+++ gatk/fqToGenotypeVcfWrap.sh
@@ -6,6 +6,10 @@
 #SBATCH --job-name=toGenotypeVcf
 #Set reference:
 REF=/data/lowelab/edotau/toGasAcu2RABS/gasAcu2RABS/gasAcu2RABS.fasta
+
+#set number of threads to use, needs to equal computer or number you request in slurm,
+#I found 8-12 usually works for me, anymore than 12 seems to slow things down
+threads=12
 module load bwa samtools/1.9-gcb01 GATK/4.1.3.0-gcb01
 #used by trim_galore
 module load cutadapt/2.3-gcb01 python/3.7.4-gcb01 pigz/2.3.4-gcb01
@@ -37,7 +41,7 @@ input2=$DIR/${PREFIX}_R2.fastq.gz
 
 echo -e "["$(date)"]\tAligning and sorting..."
 BAM=$DIR/${PREFIX}.bam
-bwa mem -t 8 $REF $1 $2 | samtools view -hb /dev/stdin | samtools sort -@ 8 -T $DIR/${PREFIX}.tmp -o $BAM
+bwa mem -t $threads $REF $1 $2 | samtools view -hb /dev/stdin | samtools sort -@ $threads -T $DIR/${PREFIX}.tmp -o $BAM
 samtools index $BAM
 
 ##Convert mapped bam into a bam that is a valid input for GATK
@@ -48,7 +52,6 @@ gVcf=$DIR/${PREFIX}.wgs.g.vcf.gz
 gatk --java-options "-Xmx16G" MarkDuplicates -I $BAM -O $markedDups -M $DIR/${PREFIX}".dup.metrics" -VALIDATION_STRINGENCY SILENT -CREATE_INDEX true
 #adds Read group information to bam alignments
 #this is how GATK differentiates between your cohort of samples
-
 #required aruguments:
 #Set real library read group if you need this field, for example, rna-seq lib, whole genome library atac, etc
 library="Stickleback"
@@ -61,4 +64,4 @@ samtools index $fixedBAM
 #Now run GATK on processed BAM
 gatk HaplotypeCaller --java-options "-Xmx32G" \
 	-I $fixedBAM -O $gVcf -R $REF \
-	-ERC GVCF --native-pair-hmm-threads 12 \
+	-ERC GVCF --native-pair-hmm-threads $threads \
