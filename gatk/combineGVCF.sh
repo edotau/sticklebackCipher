5fe48c40fbd81b75ef6f66a57fec43c690c59fa2
edotau <ericdotau@gmail.com>
2020-04-20 01:05:25 -0400

    gatk genotype vcf fixed bugs in pipeline

--- gatk/combineGVCF.sh
+++ gatk/combineGVCF.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --nodes=1
-#SBATCH --job-name=mergeGenotypeVcf
+#SBATCH --job-name=mergeGvcfToGenotype
 
 module load GATK/4.1.3.0-gcb01
 PREFIX=$1
@@ -15,12 +15,16 @@ for i in *.g.vcf.gz
 do
 	echo $i >> $samples
 done
-mergedVcf=${PREFIX}.merged.g.vcf.gz
+merged=${PREFIX}.merged.g.vcf.gz
+VCF=${PREFIX}.GENOTYPED.merged.vcf.gz
 gatk CombineGVCFs --java-options "-Xmx32g" \
 	-R /data/lowelab/edotau/toGasAcu2RABS/gasAcu2RABS/gasAcu2RABS.fasta \
 	-V $samples \
-	-O $mergedVcf \
-	-G StandardAnnotation -G AS_StandardAnnotation
+	-O $merged
 rm $samples
-sbatch /data/lowelab/edotau/sticklebackCipher/gatk/getSNPS.sh $mergedVcf
-sbatch /data/lowelab/edotau/sticklebackCipher/gatk/getIndels.sh $mergedVcf
+gatk --java-options "-Xmx30g" GenotypeGVCFs \
+	-R /data/lowelab/edotau/toGasAcu2RABS/gasAcu2RABS/gasAcu2RABS.fasta \
+	-V $merged \
+	-O $VCF
+sbatch /data/lowelab/edotau/sticklebackCipher/gatk/getSNPS.sh $VCF
+sbatch /data/lowelab/edotau/sticklebackCipher/gatk/getIndels.sh $VCF
--- gatk/getSNPS.sh
+++ gatk/getSNPS.sh
@@ -8,10 +8,9 @@ module add GATK/4.1.0.0-gcb01
 REF=/data/lowelab/edotau/toGasAcu2RABS/gasAcu2RABS/gasAcu2RABS.fasta
 VCF=$1
 PREFIX=$(basename $VCF .vcf.gz)
-rawSNP=${PREFIX}rawSNP.vcf.gz
-defaultSNP=${PREFIX}.SNP.vcf.gz
+rawSNP=${PREFIX}raw.SNP.vcf.gz
+defaultFilter=${PREFIX}.marked.basic.SNP.vcf.gz
 filterSNP=${PREFIX}.Filtered.SNP.vcf.gz
 gatk --java-options "-Xmx16g" SelectVariants -R $REF -V $VCF -select-type SNP -O $rawSNP
-gatk --java-options "-Xmx16g" VariantFiltration -R $REF -V $rawSNP --filter-expression "QD < 2.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" --filter-name "default_snp_filter" --cluster-window-size 35 --cluster-size 3 -O $defaultSNP
-gatk --java-options "-Xmx16g" SelectVariants -R $REF -V $defaultSNP --exclude-filtered -O $filterSNP
-rm $defaultSNP ${defaultSNP}.tbi
+gatk --java-options "-Xmx16g" VariantFiltration -R $REF -V $rawSNP --filter-expression "QD < 2.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" --filter-name "default_snp_filter" -O $defaultFilter
+gatk --java-options "-Xmx16g" SelectVariants -R $REF -V $defaultFilter --exclude-filtered -O $filterSNP
