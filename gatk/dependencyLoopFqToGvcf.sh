d9c13b50920ff818052051947711539dadb0c853
Eric Au <ericdotau@gmail.com>
2020-04-23 11:35:08 -0700

    Update dependencyLoopFqToGvcf.sh

--- gatk/dependencyLoopFqToGvcf.sh
+++ gatk/dependencyLoopFqToGvcf.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-for i in *_1.fq.gz
+
+gVcfMerged=stickleback.cohort
+
+for i in *R1*.{fastq,fq}*
 do
 	PREFIX=$(basename $i _1.fq.gz)
 	fqToBam=$(sbatch ./fqToBam.sh $i ${PREFIX}_2.fq.gz)
 	dupsMates=$(sbatch --dependency=afterok:$fqToBam ./bamToGenotypeVcf.sh ${PREFIX}.bam)
+	mergeGvcf=$(sbatch --dependency=singleton --job-name=$dupsMates ./combineGVCF.sh $gVcfMerged)
+	genotypeVcf=$(sbatch --dependency=$mergeGvcf ./toGenotypeVcf ${gVcfMerged}.g.vcf.gz)
+	sbatch ./getSNPs ${gVcfMerged}.vcf
+	sbatch ./getIndels ${gVcfMerged}.vcf
 done
 exit 0
