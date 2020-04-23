4a31f56fe41189fb3dfec56e34d73c48eec10ccf
Eric Au <ericdotau@gmail.com>
2020-04-23 11:10:14 -0700

    Update fqToBam.sh

--- gatk/fqToBam.sh
+++ gatk/fqToBam.sh
@@ -8,20 +8,24 @@
 set -e
 #Set reference:
 REF=/data/lowelab/edotau/toGasAcu2RABS/gasAcu2RABS/gasAcu2RABS.fasta
-module load bwa samtools/1.9-gcb01 GATK/4.1.3.0-gcb01
+module load bwa samtools/1.9-gcb01
+
 #used by trim_galore
 module load cutadapt/2.3-gcb01 python/3.7.4-gcb01 pigz/2.3.4-gcb01
 
 READ1=$1
 READ2=$2
+
+#depending on which sequencing facility you use, fastq files might
+#come in different naming conventions: ie read.fastq.gz or read.
+
+#ex. echo CL12w16-3_atac_R1.fastq.gz | rev | cut -d '_' -f 2- | rev
+#CL12w16-3_atac
+PREFIX=$(echo $READ1 | rev | cut -d '_' -f 2- | rev)
 #Final output:
-PREFIX=$(echo $READ1 | rev | cut -d '.' -f 3- | rev)
 BAM=${PREFIX}.bam
 
-
-PREFIX=$(basename $READ1 _1.fq.gz )
-DIR=${PREFIX}"to"$(basename $REF | cut -d '.' -f 1)
-
+DIR="basic_alignment"
 mkdir -p $DIR
 
 trim_galore=/data/lowelab/edotau/software/TrimGalore-0.6.5/trim_galore
@@ -34,14 +38,13 @@ input2=$DIR/${PREFIX}_R2.fastq.gz
 
 #Second Trimming of adaptors:
 /data/lowelab/software/bbmap/bbduk.sh \
-	in1=$trim1 \
-	in2=$trim2 \
-	out1=$input1 \
-	out2=$input2 \
+	in1=$trim1 in2=$trim2 \
+	out1=$input1 out2=$input2 \
 	minlen=25 qtrim=rl trimq=10 ktrim=r k=25 mink=11 hdist=1 \
 	ref=/data/lowelab/software/bbmap/resources/adapters.fa
-#MAPPING
 
+#clean directory: rm $trim1 $ trim2
+#MAPPING
 echo -e "["$(date)"]\tAligning and sorting..."
-bwa mem -t 8 $REF $1 $2 | samtools view -hb /dev/stdin | samtools sort -@ 4 -T $DIR/${PREFIX}.tmp -o $BAM
+bwa mem -t 8 $REF $1 $2| samtools sort -@ 8 -T $DIR/${PREFIX}.tmp /dev/stdin > $BAM
 samtools index $BAM
