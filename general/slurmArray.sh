f0f53bfb7633cc994ee5b1c5abea68cf666c9b88
Eric Au <ericdotau@gmail.com>
2020-06-29 00:32:54 -0700

    Update slurmArray.sh

--- general/slurmArray.sh
+++ general/slurmArray.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
+REF=$1
+BAM=$2
+sbatch ./gatkHaplotype.sh $BAM $REF
+
+#!/bin/bash
 #SBATCH --mem=12G
-#SBATCH --cpus-per-task=4 --ntasks=2
+#SBATCH --cpus-per-task=4 --ntasks=3
 #SBATCH --array=1-100
 #SBATCH --time=0-03
 #SBATCH --nodes=1
-#SBATCH --exclude=dl-01,x2-02-2,x2-04-4
+#SBATCH --exclude=dl-01,x2-02-2,c1-10-3
 
 i=$SLURM_ARRAY_TASK_ID
-
 bam=$1
+fa=$2
+faidx=$fa.fai
+#LEN=$3
 
-LEN=`wc -l $fasta.fai | awk '{print $1}'`
+#####tasks=$SLURM_CPUS_ON_NODE
+LEN="$((wc -l $contigs))"
 
 # Perform function every %100 = $i th line
 for j in $(seq $i 100 $LEN )
 do
     echo $j
-    contig=`sed -n ${j}p $fasta.fai | awk '{print $1}'`
+    contig=`sed -n ${j}p $faidx | awk '{print $1}'`
     contig_no_pipe=`echo $contig | sed 's/|/_/g'`
-    end=`sed -n ${j}p $fasta.fai | awk '{print $2}'`
+    end=`sed -n ${j}p $faidx | awk '{print $2}'`
     
-    #chunk=$end%4
-    s2=$start+$chunk, s3=$s2+$chunk , s4=$s3+$chunk
+    #####LEN="$((wc -l $faidx)+$tasks-1/$tasks))"
+
+    chunk=$(($LEN / 3))
+    #last chuck includes remainder
+    s2=$start+$chunk, s3=$s2+$chunk , s4=$s3+$end
     gatk $bam -L $contig:$start-$s2 & gatk $bam -L $contig:$s2-$s3 & gatk $bam -L $contig:$s3-$end
 done
+exit 0
