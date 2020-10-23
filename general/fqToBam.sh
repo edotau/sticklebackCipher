ba0f3ada05a109992a11f7f678f9d1c8d8b52bdb
Eric Au <ericdotau@gmail.com>
2020-10-23 13:34:57 -0400

    Update fqToBam.sh

--- general/fqToBam.sh
+++ general/fqToBam.sh
@@ -37,8 +37,41 @@ input2=$fastqDIR/${PREFIX}_R2.fastq.gz
 	minlen=25 qtrim=rl trimq=10 ktrim=r k=25 mink=11 hdist=1 \
 	ref=/data/lowelab/software/bbmap/resources/adapters.fa
 
-#clean directory: rm $trim1 $ trim2
 #MAPPING
+output=$DIR/${PREFIX}.FINAL.bam
+
+SORT_THREADS=$(($SLURM_CPUS_ON_NODE / 4))
+
+THREADS=$(($SLURM_CPUS_ON_NODE - $SORT_THREADS))
+
+
 echo -e "["$(date)"]\tAligning and sorting..."
-bwa mem -t 8 $REF $1 $2| samtools sort -@ 8 -T $DIR/${PREFIX}.tmp /dev/stdin > $BAM
-samtools index $BAM
+echo "
+bwa mem -t $THREADS $REF $READ1 $READ2 | samtools sort -@ $SORT_THREADS -T $DIR/${PREFIX}.tmp -m 1G /dev/stdin > $BAM"
+bwa mem -t $THREADS $REF $READ1 $READ2 | samtools sort -@ $SORT_THREADS -T $DIR/${PREFIX}.tmp -m 1G /dev/stdin > $BAM
+
+echo "
+samtools index -@ $SLURM_CPUS_ON_NODE $BAM"
+samtools index -@ $SLURM_CPUS_ON_NODE $BAM
+
+module load GATK/4.1.3.0-gcb01
+markedDups=$DIR/${PREFIX}.markedDups.bam
+
+echo "
+gatk MarkDuplicates -I $BAM -O $markedDups -M /dev/null -VALIDATION_STRINGENCY SILENT -AS"
+gatk MarkDuplicates -I $BAM -O $markedDups -M /dev/null -VALIDATION_STRINGENCY SILENT -AS
+samtools index -@ $SORT_THREADS $markedDups
+
+library=$(echo $PREFIX | cut -d '_' -f 2 | cut -d '.' -f 1)
+unit=$(samtools view $BAM | head -n 1 | cut -f 1 | cut -d ':' -f 3,4 | tr ':' '.')
+platform="HiSeqX"
+sampleID=$PREFIX
+
+
+echo "
+gatk AddOrReplaceReadGroups -I $markedDups -O /dev/stdout -LB $library -PL $platform -PU $unit -SM $sampleID | gatk FixMateInformation -I /dev/stdin -O $output -ADD_MATE_CIGAR true -IGNORE_MISSING_MATES true -TMP_DIR $DIR/"fixmate.tmp" -AS"
+gatk AddOrReplaceReadGroups -I $markedDups -O /dev/stdout -LB $library -PL $platform -PU $unit -SM $sampleID | gatk FixMateInformation -I /dev/stdin -O $output -ADD_MATE_CIGAR true -IGNORE_MISSING_MATES true -TMP_DIR $DIR/"fixmate.tmp" -AS
+
+samtools index -@ $SLURM_CPUS_ON_NODE $output
+
+exit 0
