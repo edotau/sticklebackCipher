3fa4fc36e9f81d79e5a3bc33a9634efb55b0324b
Eric Au <ericdotau@gmail.com>
2020-12-28 12:53:23 -0800

    Update chainsToNets.sh

--- lastz/chainsToNets.sh
+++ lastz/chainsToNets.sh
@@ -1,46 +1,84 @@
 #!/bin/sh
-#SBATCH --mem=16G
+#SBATCH --mem=24G
 #SBATCH --cpus-per-task=1
-#SBATCH --ntasks=1
-#SBATCH --time=0-06
+#SBATCH --ntasks=2
+#SBATCH --time=0-02
 #SBATCH --nodes=1
 set -e
-module add kentUtils/v302-gcb01
-PREFIX=$1
-targetContigs=$2
-queryContigs=$3
-DIR=${4%/}
+
+export PATH=/data/lowelab/edotau/kentUtils/:/data/lowelab/edotau/bin/envs/htslib/bin/:$PATH
+PREFIX=rabsTHREEbepaSpine
+
+targetContigs=$1
+queryContigs=$2
+
+DIR=/data/lowelab/edotau/rabsTHREEspine/lastz/axtChains/chains
 
 targetPREFIX=$(basename $targetContigs .fa)
 queryPREFIX=$(basename $queryContigs .fa)
 
 target=${targetPREFIX}.sizes
 query=${queryPREFIX}.sizes
+echo "
+faSize -detailed $targetContigs > $target"
 faSize -detailed $targetContigs > $target
+
+echo "
+faSize -detailed $queryContigs > $query"
 faSize -detailed $queryContigs > $query
 
 bitTarget=${targetPREFIX}.2bit
 bitQuery=${queryPREFIX}.2bit
+
+echo "
+faToTwoBit $targetContigs $bitTarget"
 faToTwoBit $targetContigs $bitTarget
+
+
+echo "faToTwoBit $queryContigs $bitQuery"
 faToTwoBit $queryContigs $bitQuery
 
+echo "
+chainMergeSort $DIR/*.chain | chainSplit chainMerge stdin -lump=50"
 chainMergeSort $DIR/*.chain | chainSplit chainMerge stdin -lump=50
+
+
+echo "
+cat chainMerge/*.chain > ${PREFIX}.all.chain"
 cat chainMerge/*.chain > ${PREFIX}.all.chain
-chainSort ${PREFIX}.all.chain ${PREFIX}.all.sorted.chain
+
+echo "
+chainSort ${PREFIX}.all.chain ${PREFIX}.all.sorted.chain; cat ${PREFIX}.all.sorted.chain | grep -v '#' > ${PREFIX}.all.chain"
+chainSort ${PREFIX}.all.chain ${PREFIX}.all.sorted.chain; cat ${PREFIX}.all.sorted.chain | grep -v '#' > ${PREFIX}.all.chain
 rm -r chainMerge
 
-chainPreNet ${PREFIX}.all.sorted.chain $target $query ${PREFIX}ChainPreNet.chain
+echo "
+chainPreNet ${PREFIX}.all.chain $target $query ${PREFIX}ChainPreNet.chain"
+chainPreNet ${PREFIX}.all.chain $target $query ${PREFIX}ChainPreNet.chain
+
 net=${PREFIX}.net
-chainNet ${PREFIX}ChainPreNet.chain $target $query /dev/stdout /dev/null | netSyntenic /dev/stdin $net
-#${PREFIX}_syntenic_netted_chain.net
-netFilter -minScore=500000 $net > ${PREFIX}Filtered.net
 
-netChainSubset ${PREFIX}Filtered.net ${PREFIX}.all.sorted.chain ${PREFIX}Netted.chain
-axt=${PREFIX}NettedChain.axt
-netToAxt ${PREFIX}Filtered.net ${PREFIX}.all.sorted.chain $bitTarget $bitQuery $axt
-samfile=${PREFIX}.sam
-/data/lowelab/edotau/gonomics/cmd/axtSam/axtSam -chrom $target $axt $samfile
+echo "
+chainNet ${PREFIX}ChainPreNet.chain $target $query /dev/stdout /dev/null | netSyntenic /dev/stdin $net | netFilter /dev/stdout -minScore=1000000 > $net"
+chainNet ${PREFIX}ChainPreNet.chain $target $query /dev/stdout /dev/null | netSyntenic /dev/stdin $net | netFilter /dev/stdout -minScore=1000000 > $net
 
-module add samtools
-samtools sort -@ $SLURM_CPUS_ON_NODE $samfile > ${PREFIX}.bam
+echo "
+netChainSubset $net ${PREFIX}ChainPreNet.chain ${PREFIX}.netted.chain"
+netChainSubset $net ${PREFIX}ChainPreNet.chain ${PREFIX}.netted.chain
+axt=${PREFIX}.netted.chains.axt
+
+echo "
+netToAxt $net ${PREFIX}.all.chain $bitTarget $bitQuery $axt"
+netToAxt $net ${PREFIX}.all.chain $bitTarget $bitQuery $axt
+
+echo "
+axtDropOverlap $axt $target $query ${axt}.tmp; mv ${axt}.tmp $axt"
+axtDropOverlap $axt $target $query ${axt}.tmp; mv ${axt}.tmp $axt
+
+
+echo "
+/data/lowelab/edotau/golang/src/github.com/vertgenlab/gonomics/cmd/axtSam -chrom $target $axt /dev/stdout | samtools sort -@ $SLURM_CPUS_ON_NODE /dev/stdin > ${PREFIX}.bam"
+/data/lowelab/edotau/golang/src/github.com/vertgenlab/gonomics/cmd/axtSam -chrom $target $axt /dev/stdout | samtools sort -@ $SLURM_CPUS_ON_NODE /dev/stdin > ${PREFIX}.bam
+echo "
+samtools index ${PREFIX}.bam"
 samtools index ${PREFIX}.bam
