9792bd85e58d27e17e2d0a77a49b889a8b2ab0c9
edotau <ericdotau@gmail.com>
2020-12-31 23:25:58 -0500

    added lastz.sh script for publication

--- lastz/lastz.sh
+++ lastz/lastz.sh
@@ -1,31 +1,41 @@
 #!/bin/sh
-#SBATCH --mem=8G
+#SBATCH --mem=4G
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --nodes=1
-#SBATCH --exclude=dl-01
 set -e
+# Set Path to kentUtils or load kentUtils module if available on your cluster:
+export PATH=/data/lowelab/edotau/kentUtils/:$PATH
+
+# Set lastz or add to path
+lastz=/data/lowelab/edotau/software/lastz-distrib-1.04.00/src/lastz
+
+# Determine Scoring matrix
+scoreMatrix=/data/lowelab/edotau/software/lastz-distrib-1.04.00/humanChimpMatrix.txt
+
+# target or reference fasta MUST be a single record fasta sequence:
 target=$1
-query=$2
+# query fasta can be multi records
+query=$2  # fasta query
+
+if [[ "$#" -lt 2 ]]; then
+    echo "Usage: ./lastz.sh target.fa query.fa"
+    exit 0
+fi
+
 PREFIX=$(basename $target .fa)_$(basename $query .fa)
 axt=${PREFIX}.axt
 
-echo "Target=$target, Query=$query"
-export PATH=/data/lowelab/edotau/kentUtils/:$PATH
-
-lastz=/data/lowelab/edotau/software/lastz-distrib-1.04.00/src/lastz
-humanChimp=/data/lowelab/edotau/software/lastz-distrib-1.04.00/humanChimpMatrix.txt
-
 echo "
-$lastz $target $query --format=axt --output=$axt --scores=$humanChimp O=600 E=150 T=2 M=254 K=4500 L=4500 Y=15000 C=0
+$lastz $target $query --format=axt --output=$axt --scores=$scoreMatrix O=600 E=150 T=2 M=254 K=4500 L=4500 Y=15000 C=0
 "
-$lastz $target $query --format=axt --output=$axt --scores=$humanChimp O=600 E=150 T=2 M=254 K=4500 L=4500 Y=15000 C=0 
+$lastz $target $query --format=axt --output=$axt --scores=$scoreMatrix O=600 E=150 T=2 M=254 K=4500 L=4500 Y=15000 C=0 
 
 echo "finished lastz"
 
 echo "
-axtChain -linearGap=medium -scoreScheme=$humanChimp $axt -faT $target -faQ $query /dev/stdout | chainSort /dev/stdin ${PREFIX}.chain
+axtChain -linearGap=medium -scoreScheme=$scoreMatrix $axt -faT $target -faQ $query /dev/stdout | chainSort /dev/stdin ${PREFIX}.chain
 "
-axtChain -linearGap=medium -scoreScheme=$humanChimp $axt -faT $target -faQ $query /dev/stdout | chainSort /dev/stdin ${PREFIX}.chain
+axtChain -linearGap=medium -scoreScheme=$scoreMatrix $axt -faT $target -fta target or refQ $query /dev/stdout | chainSort /dev/stdin ${PREFIX}.chain
 
 echo "DONE"
