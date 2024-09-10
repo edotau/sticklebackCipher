e057a37224ca290b7e3080f799daea233621f8df
Eric Au <ericdotau@gmail.com>
2024-09-10 05:04:16 -0400

    fix: usage

--- geneAnnotation/gtfLiftover.sh
+++ geneAnnotation/gtfLiftover.sh
@@ -1,21 +1,20 @@
-#!/bin/bash
-set -e
+#!/bin/bash -e
 
 # Input arguments
-GTF=$1
-TARGET_CHROM_SIZES=$2
-CHAIN=$3
-QUERY=$4
+TARGET=$1
+CHAIN=$2
+GTF=$3
+OUTPUT=$4
 
 if [[ "$#" -lt 4 ]]; then
     echo "Usage:
-        $0 annotations.gtf target.sizes target.chain query.fa
+        $(basename "$0") target.fa target.chain annotations.gtf output
     "
     exit 1
 fi
 
 # Set required paths, tools and references
-KENT_UTILS="/home"
+KENT_UTILS="/home/kentUtils/bin"
 
 # Append $KENT_UTILS or set binary directly
 gtfToGenePred="$KENT_UTILS/gtfToGenePred"
@@ -24,48 +23,19 @@ pslMap="$KENT_UTILS/pslMap"
 pslToBed="$KENT_UTILS/pslToBed"
 bedToGenePred="$KENT_UTILS/bedToGenePred"
 genePredToGtf="$KENT_UTILS/genePredToGtf"
+faSize="$KENT_UTILS/faSize"
 
 # Check if all tools exist and are executable
-if ! [ -x "$gtfToGenePred" ]; then
-    echo "Error: gtfToGenePred not found or not executable at $gtfToGenePred"
-    exit 1
-fi
-
-if ! [ -x "$genePredToPsl" ]; then
-    echo "Error: genePredToPsl not found or not executable at $genePredToPsl"
-    exit 1
-fi
-
-if ! [ -x "$pslMap" ]; then
-    echo "Error: pslMap not found or not executable at $pslMap"
-    exit 1
-fi
-
-if ! [ -x "$pslToBed" ]; then
-    echo "Error: pslToBed not found or not executable at $pslToBed"
-    exit 1
-fi
-
-if ! [ -x "$bedToGenePred" ]; then
-    echo "Error: bedToGenePred not found or not executable at $bedToGenePred"
-    exit 1
-fi
-
-if ! [ -x "$genePredToGtf" ]; then
-    echo "Error: genePredToGtf not found or not executable at $genePredToGtf"
-    exit 1
-fi
-
-
-# Check if the chrom sizes file exists
-if ! [ -f "$TARGET_CHROM_SIZES" ]; then
-    echo "Error: Chrom sizes file not found at $TARGET_CHROM_SIZES"
-    exit 1
-fi
+for i in "$gtfToGenePred" "$genePredToPsl" "$pslMap" "$pslToBed" "$bedToGenePred" "$genePredToGtf" "$faSize"; do
+    if ! [ -x "$i" ]; then
+        echo "Error: $(basename "$i") not found or not executable at $i"
+        exit 1
+    fi
+done
 
 # Derived variables
 PREFIX=$(basename "$GTF" .gtf)
-OUTPUT=$(basename "$QUERY" .fa)
+TCHROM=$(basename "$TARGET" | sed -E 's/\.(fa|fa\.gz|fasta|fasta\.gz)$//')
 GP_FILE="${PREFIX}.gp"
 PSL_FILE="${PREFIX}.psl"
 MAPPED_PSL_FILE="${OUTPUT}.psl"
@@ -73,44 +43,47 @@ OUTPUT_BED="${OUTPUT}.bed"
 OUTPUT_GP="${OUTPUT}.gp"
 OUTPUT_GTF="${OUTPUT}.gtf"
 
-# Step 1: Convert GTF to GenePred format
+# Step 1: Create target chromSizes from target .fa
+if ! [ -e "$TCHROM" ]; then
+    echo "Creating chromSizes format..."
+    $faSize -detailed "$TARGET" > "$TCHROM"
+fi
+
+# Step 2: Convert GTF to GenePred format
 if ! [ -e "$GP_FILE" ]; then
     echo "Converting $GTF to GenePred format..."
     $gtfToGenePred -genePredExt -includeVersion "$GTF" "$GP_FILE"
 fi
 
-# Step 2: Convert GenePred to PSL format
+# Step 3: Convert GenePred to PSL format
 if ! [ -e "$PSL_FILE" ]; then
     echo "Converting $GP_FILE to PSL format..."
-    $genePredToPsl "$TARGET_CHROM_SIZES" "$GP_FILE" "$PSL_FILE"
+    $genePredToPsl "$TCHROM" "$GP_FILE" "$PSL_FILE"
 fi
 
-# Step 3: Map PSL using chain file
+# Step 4: Map PSL using chain file
 if ! [ -e "$MAPPED_PSL_FILE" ]; then
     echo "Mapping $PSL_FILE using chain file..."
     $pslMap -chainMapFile -swapMap "$PSL_FILE" "$CHAIN" "$MAPPED_PSL_FILE"
 fi
 
-# Step 4: Convert mapped PSL to BED format
+# Step 5: Convert mapped PSL to BED format
 if ! [ -e "$OUTPUT_BED" ]; then
     echo "Converting $MAPPED_PSL_FILE to BED format..."
     $pslToBed "$MAPPED_PSL_FILE" "$OUTPUT_BED"
 fi
 
-# Step 5: Convert BED to GenePred format
+# Step 6: Convert BED to GenePred format
 if ! [ -e "$OUTPUT_GP" ]; then
     echo "Converting $OUTPUT_BED to GenePred format..."
     $bedToGenePred "$OUTPUT_BED" "$OUTPUT_GP"
 fi
 
-# Step 6: Convert GenePred to GTF format
+# Step 7: Convert GenePred to GTF format
 if ! [ -e "$OUTPUT_GTF" ]; then
     echo "Converting $OUTPUT_GP to GTF format..."
     $genePredToGtf file "$OUTPUT_GTF"
 fi
-
-# Clean up intermediate files
-rm -f "$GP_FILE" "$PSL_FILE"
 
 # Final Output
 echo "Completed successfully!
@@ -120,3 +93,6 @@ Generated files:
     $OUTPUT_GP
     $OUTPUT_GTF
 "
+
+# Clean up intermediate files
+rm -f "$TCHROM" "$GP_FILE" "$PSL_FILE"
